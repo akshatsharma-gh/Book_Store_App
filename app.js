2170ebccb9bc91973c033adb56acfda2f606d2f5
akshatsharma-gh <akshatsharma8826@gmail.com>
2023-04-21 18:51:09 +0530

    Backend Uploaded

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router); // localhost:5000/books
+
+mongoose
+  .connect(
+    "mongodb+srv://akshatsharma8826:A!kshatm@cluster0.2cy5x0b.mongodb.net/bookstore"
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
